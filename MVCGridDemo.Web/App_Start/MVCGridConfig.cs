25cd2d70c8f349768ceb7862e0c751955f7b6514
joeharrison714 <bitbucket@joeharrison.com>
2015-07-21 19:31:49 -0400

    no message

--- MVCGridDemo.Web/App_Start/MVCGridConfig.cs
+++ MVCGridDemo.Web/App_Start/MVCGridConfig.cs
@@ -33,19 +33,14 @@ namespace MVCGridDemo.Web
                     cols.Add("Volume", "Volume", m => m.Volume.ToString("N0")).WithSorting(true);
                     cols.Add("PublicationDate", "Publ. Date", m => m.PublicationDate.ToShortDateString()).WithSorting(true);
 
-                    cols.Add("Edit", "Edit", m => m.Index.ToString()).WithSorting(true)
-                        .WithValueTemplate("<a href='/edit/{Value}' class='btn btn-primary' role='button'>{Model.Author}</a>", false);
+                    cols.Add("Edit", "Edit", m => m.Index.ToString()).WithSorting(false)
+                        .WithValueTemplate("<a href='/edit/{Value}' class='btn btn-primary' role='button'>Edit</a>", false);
 
-                    // Add your columns here
-                    //cols.Add().WithColumnName("UniqueColumnName")
-                    //    .WithHeaderText("Any Header")
-                    //    .WithValueExpression(i => i.YourProperty); // use the Value Expression to return the cell text for this column
-                    //cols.Add().WithColumnName("UrlExample")
-                    //    .WithHeaderText("Edit")
-                    //    .WithValueExpression((i, c) => c.UrlHelper.Action("detail", "demo", new { id = i.Id }));
+                    cols.Add("Delete", "Delete", m => m.Index.ToString()).WithSorting(false)
+                        .WithValueTemplate("<a href='/edit/{Value}' class='btn btn-danger' role='button'>Delete</a>", false);
                 })
                 .WithRowCssClassExpression(b => {
-                    if (b.Volume > 3000000)
+                    if (b.PublicationDate.Year >= (DateTime.Now.Year - 10))
                     {
                         return "success";
                     }
@@ -53,10 +48,6 @@ namespace MVCGridDemo.Web
                 })
                 .WithRetrieveDataMethod((context) =>
                 {
-                    // Query your data here. Obey Ordering, paging and filtering parameters given in the context.QueryOptions.
-                    // Use Entity Framework, a module from your IoC Container, or any other method.
-                    // Return QueryResult object containing IEnumerable<YouModelItem>
-
                     var bookService = DependencyResolver.Current.GetService<IBookService>();
 
                     BookOrderBy orderBy;
