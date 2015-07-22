1ea632075e2abda118973010107f76eb782bc5cd
joeharrison714 <bitbucket@joeharrison.com>
2015-07-21 20:42:24 -0400

    Demo Step 2.0

---
+++ MVCGridDemo.Web/App_Start/MVCGridConfig.cs
@@ -0,0 +1,60 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MVCGridDemo.Web.MVCGridConfig), "RegisterGrids")]
+
+namespace MVCGridDemo.Web
+{
+    using System;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using MVCGrid.Models;
+    using MVCGrid.Web;
+    using MVCGridDemo.Model;
+    using MVCGridDemo.Interfaces;
+
+    public static class MVCGridConfig 
+    {
+        public static void RegisterGrids()
+        {
+            
+            MVCGridDefinitionTable.Add("BookGrid", new MVCGridBuilder<Book>()
+                .WithAuthorizationType(AuthorizationType.AllowAnonymous)
+                .AddColumns(cols =>
+                {
+                    cols.Add("Title", "Title", b => b.Title);
+                    cols.Add("Author", "Author", b => b.Author);
+
+                    // Add your columns here
+                    //cols.Add().WithColumnName("UniqueColumnName")
+                    //    .WithHeaderText("Any Header")
+                    //    .WithValueExpression(i => i.YourProperty); // use the Value Expression to return the cell text for this column
+                    //cols.Add().WithColumnName("UrlExample")
+                    //    .WithHeaderText("Edit")
+                    //    .WithValueExpression((i, c) => c.UrlHelper.Action("detail", "demo", new { id = i.Id }));
+                })
+                .WithRetrieveDataMethod((context) =>
+                {
+                    // Query your data here. Obey Ordering, paging and filtering parameters given in the context.QueryOptions.
+                    // Use Entity Framework, a module from your IoC Container, or any other method.
+                    // Return QueryResult object containing IEnumerable<YouModelItem>
+
+                    IBookService bookService = DependencyResolver.Current.GetService<IBookService>();
+
+                    var options = context.QueryOptions;
+
+                    int totalRecords;
+                    var results = bookService.GetBooks(out totalRecords, options.GetLimitOffset(), options.GetLimitRowcount(), options.SortColumnName, options.SortDirection == SortDirection.Dsc);
+
+                    return new QueryResult<Book>()
+                    {
+                        Items = results,
+                        TotalRecords = totalRecords // if paging is enabled, return the total number of records of all pages
+                    };
+
+                })
+            );
+            
+        }
+    }
+}
